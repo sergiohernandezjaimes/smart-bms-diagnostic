a1f84b07adc24013d5f235cc44f79a19fa36ef3e
Sergio <sergiohernandezjaimes@gmail.com>
2025-05-23 21:00:38 -0700

    Update dashboard.py
    Upload & Filter
    New dashboard:
    A file uploader (CSV only)
    A dropdown to filter by fault type
    An optional date range filter
    Fixed bug comments

--- dashboard.py
+++ dashboard.py
@@ -5,38 +5,53 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Load data from CSV
-def load_data(csv_file='bms_log.csv'):
+#Load data from uploaded file or default CSV
+def load_data(uploaded_file=None, default_file='bms_log.csv'):
   try:
-    df = pd.read_csv(csv_file, parse_dates=['timestamp'])
-    return df
-  except FileNotFoundError:
-    st.error("Log file not found. Please run the simulate first.")
+    if uploaded_file is not None:
+      df = pd.read_csv(uploaded_file, parse_dates=['timestamp'])
+  else:
+      df = pd.read_csv(defualt_file, parse_dates=['timestamp'])
+  return df
+except FileNotFoundError:
+    st.error("Log file not found. Please run the simulate or upload a log file.")
     return pd.DataFrame()
 
 #Display fault status with colors
 def status_color(status):
-  if "FAULT" in status:
-      return 'red'
+    if "FAULT" in status:
+        return 'red'
     return 'green'
 
 # Main app
 st.set_page_config(page_title="Smart BMS Dashboard", layout="wide")
-st.title("🔋 Smart BMS Diagnostic Dashboard")
+st.title("Smart BMS Diagnostic Dashboard")
 
-data = load_data()
+st.sidebar.header("Data Options")
+uploaded_file = st.sidebar.file_uploader("Upload a BMS log file (CSV)", type="csv")
+data = load_data(uploaded_file)
 
 if not data.empty:
-st.subheader("📊 BMS Time-Series Data")
-st.line_chart(data[['soc, 'voltage', 'current', 'temperature']])
+  #Optional: Filter by fault type
+  unique_statuses = data['status'].unique().tolist()
+  selected_status = st.sidebar.selectbar.selectbox("Filter by Status", ["All"] + unique_statuses)
 
-st.subheader(📋 Detailed Log Table")
-colored_status = data['status].apply(lamba x: f"<span style='color:{status_color(x)}'>{x}</span>")
-styled_df = data.copy()
-styled_df['status] = colored_status
-st.write(styled_df.tohtml(escape=False), unsafe_allow_html=True)
+  if selected_status != "All":
+      data = data[data['status'] == selected_status]
 
-st.subheader("⚠️ Fault Summary")
-fault_counts = data['status'].value_counts()
-st.bar_chart(faults_counts)
+  st.subheader("📊 BMS Time-Series Data")
+  st.line_chart(data[['soc', 'voltage', 'current', 'temperature']])
+
+  st.subheader("📋 Detailed Log Table")
+  colored_status = data['status'].apply(lamba x: f"<span style='color:{status_color(x)}'>{x}</span>")
+  styled_df = data.copy()
+  styled_df['status] = colored_status
+  st.write(styled_df.to_html(escape=False), unsafe_allow_html=True)
+
+  st.subheader("Fault Summary")
+  fault_counts = data['status'].value_counts()
+  st.bar_chart(faults_counts)
+else:
+  st.info("Please upload a log file or run the simulator to generate data.")
+
                     
